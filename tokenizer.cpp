e0e199f130f321d44a104a3e826bc8fda8995f7c
Gaurav-Bhagat <110767302+Gaurav-Bhagat@users.noreply.github.com>
2025-01-20 15:40:59 +0530

    tokenizer.cpp

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -3,21 +3,26 @@
 #include <string>
 #include <fstream> // For file purposes
 using namespace std;
+vector<pair<string,pair<int,int>>> operators;
+vector<pair<string,pair<int,int>>> keyword;
+vector<pair<string,pair<int,int>>> variables;
+vector<pair<string,pair<int,int>>> nums;
+vector<pair<string,pair<int,int>>> punct;
 
 void extractToken(string s, int row, int col) {
     regex var("^[a-zA-Z_][a-zA-Z0-9_]*$");
     regex num("^[+-]?\\d+(\\.\\d+)?([eE][+-]?\\d+)?$");
-    regex key(R"(\b(int|char|void|if|else|for|float|while|return|break|continue|switch|case|default|struct|union|typedef|namespace|public|private|protected|class|new|delete|try|catch|throw|inline|virtual|static|const|volatile|friend|explicit|auto|long|short|signed|unsigned|true|false|nullptr)\b)");
+    regex key(R"(\b(int|char|void|if|else|for|float|while|return|break|printf|continue|switch|case|default|struct|union|typedef|namespace|public|private|protected|class|new|delete|try|catch|throw|inline|virtual|static|const|volatile|friend|explicit|auto|long|short|signed|unsigned|true|false|nullptr)\b)");
     regex op(R"([+\-*/%=!&|<>^~])");
 
     if (regex_match(s, op)) {
-        cout << "Operator: " << s << " at (" << row << "," << col << ")" << endl;
+        operators.push_back(make_pair(s, make_pair(row, col)));
     } else if (regex_match(s, key)) {
-        cout << "Keyword: " << s << " at (" << row << "," << col << ")" << endl;
+        keyword.push_back(make_pair(s, make_pair(row, col)));
     } else if (regex_match(s, var)) {
-        cout << "Variable: " << s << " at (" << row << "," << col << ")" << endl;
+        variables.push_back(make_pair(s, make_pair(row, col)));
     } else if (regex_match(s, num)) {
-        cout << "Number: " << s << " at (" << row << "," << col << ")" << endl;
+        nums.push_back(make_pair(s, make_pair(row, col)));
     } else {
         cout << "Invalid Token: " << s << " at (" << row << "," << col << ")" << endl;
     }
@@ -45,9 +50,7 @@ int main() {
                     insideToken = false;
                 }
                 if (!isspace(line[i])) {
-                    // string punct(1, line[i]);
-                    // extractToken(punct, row, col);
-                    cout<<"Punct: "<<line[i]<<" at "<<row<<" "<<col<<endl;
+                    punct.push_back(make_pair(string(1, line[i]), make_pair(row, col)));
                 }
                 col++;
             } else {
@@ -64,6 +67,57 @@ int main() {
         row++;
     }
 
+    cout<<"---------------------------------------------------"<<endl;
+    if(keyword.size() > 0){
+        cout<<"------------------Keywords-------------"<<endl;
+        for(auto p : keyword){
+            string str = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
+            cout <<  str << " at Row: " << row << "  Col: " << col << endl;
+        }
+    }
+    cout<<"---------------------------------------------------"<<endl;
+    if(variables.size() > 0){  
+        cout<<"------------------Variables-------------"<<endl;
+        for(auto p : variables){
+            string str = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
+            cout <<  str << " at Row: " << row << "  Col: " << col << endl;
+        }
+    }
+    cout<<"---------------------------------------------------"<<endl;
+    if(operators.size() > 0){
+        cout<<"------------------Operators-------------"<<endl;
+        for(auto p : operators){
+            string str = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
+            cout <<  str << " at Row: " << row << "  Col: " << col << endl;
+        }
+    }
+    cout<<"---------------------------------------------------"<<endl;
+    if(nums.size() > 0){
+        cout<<"------------------Numbers-------------"<<endl;
+        for(auto p : nums){
+            string str = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
+            cout <<  str << " at Row: " << row << "  Col: " << col << endl;
+        }
+    }
+    cout<<"---------------------------------------------------"<<endl;
+    if(punct.size() > 0){
+        cout<<"------------------Punctuations-------------"<<endl;
+        for(auto p : punct){
+            string str = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
+            cout <<  str << " at Row: " << row << "  Col: " << col << endl;
+        }
+    }
+    file.close(); 
     cout << "Done" << endl;
     return 0;
 }
